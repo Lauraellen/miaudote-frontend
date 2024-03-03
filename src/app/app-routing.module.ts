0da56b9868855b7a7b0a3564ee51ddf7bb1e4ea9
laura.souza <laura.souza@gclaims.com.br>
2024-03-03 18:06:16 -0300

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: "adote",
+  component: MainScreenComponent
+}, {
+  path: "",
+  redirectTo: "adote",
+  pathMatch: "full"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

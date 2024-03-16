eb86c6d1909700c4574880d3c5ceb3c851699998
laura.souza <laura.souza@gclaims.com.br>
2024-03-16 10:07:54 -0300

    ajuste do getPets

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
 import { PetService } from '../services/pet/pet.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 
 declare var $: any;
 import 'slick-carousel';
@@ -10,101 +9,24 @@ import 'slick-carousel';
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.css']
 })
-export class MainScreenComponent implements OnInit, AfterViewInit{
+export class MainScreenComponent implements OnInit {
 
-  //listPets: any[] = [];
-
-  listPets: any[] = [
-    {
-      name: 'Floquinho',
-      breed: 'Vira-lata',
-      age: '5 anos',
-      city: 'Cambuí - MG',
-      description: 'Minha avó faleceu e não podemos cuidar',
-      photos: [
-        { url: '/assets/images/golden-photo-01.jpeg', alt: 'Photo 1' },
-        { url: '/assets/images/golden-photo-02.jpg', alt: 'Photo 2' },
-        { url: '/assets/images/golden-photo-03.jpeg', alt: 'Photo 3' },
-        { url: '/assets/images/golden-photo-04.jpg', alt: 'Photo 4' }
-      ]
-
-    },
-    {
-      name: 'Mili',
-      breed: 'Siamês',
-      age: 'indefinido',
-      city: 'Cambuí - MG',
-      description: 'Encontrei na rua e não posso ficar',
-      photos: [
-        { url: '/assets/images/golden-photo-01.jpeg', alt: 'Photo 1' },
-        { url: '/assets/images/golden-photo-02.jpg', alt: 'Photo 2' },
-        { url: '/assets/images/golden-photo-03.jpeg', alt: 'Photo 3' },
-        { url: '/assets/images/golden-photo-04.jpg', alt: 'Photo 4' }
-      ]
-    },
-    {
-      name: 'Pantera',
-      breed: 'Shih-tzu',
-      age: '2 anos',
-      city: 'Cambuí - MG',
-      description: 'Foi resgatada',
-      photos: [
-        { url: '/assets/images/golden-photo-01.jpeg', alt: 'Photo 1' },
-        { url: '/assets/images/golden-photo-02.jpg', alt: 'Photo 2' },
-        { url: '/assets/images/golden-photo-03.jpeg', alt: 'Photo 3' },
-        { url: '/assets/images/golden-photo-04.jpg', alt: 'Photo 4' }
-      ]
-    },
-    {
-      name: 'Mel',
-      photos: [
-        { url: '/assets/images/golden-photo-01.jpeg', alt: 'Photo 1' },
-        { url: '/assets/images/golden-photo-02.jpg', alt: 'Photo 2' },
-        { url: '/assets/images/golden-photo-03.jpeg', alt: 'Photo 3' },
-        { url: '/assets/images/golden-photo-04.jpg', alt: 'Photo 4' }
-      ]
-    },
-  ]
-  slickCarousels: any[] = []; // Inicializa a propriedade slickCarousels como uma array vazia
+  listPets: any;
+  loadingPets: boolean = false;
+  slickCarousels: any[] = [];
 
   constructor(
     private elementRef: ElementRef,
     private petService: PetService,
-  ) {}
+    private cdr: ChangeDetectorRef
+
+  ) { }
 
   ngOnInit(): void {
-    // this.getPets().subscribe(
-    //   (pets: any[]) => {
-    //     this.listPets = pets;
-    //   },
-    //   (error) => {
-    //     console.error('Erro ao carregar pets', error);
-    //   }
-    // );
+    this.getPets();
   }
 
-  // ngAfterViewInit() {
-  //   // this.slickCarousels.forEach((carousel, index) => {
-  //   //   $(carousel).slick({
-  //   //     slidesToShow: 1,
-  //   //     slidesToScroll: 1,
-  //   //     dots: true,
-  //   //     arrows: true,
-  //   //     autoplay: false,
-  //   //     infinite: true
-  //   //   });
-  //   // });
-  //   $(this.elementRef.nativeElement.querySelector('.slick-carousel')).slick({
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1,
-  //     dots: true,
-  //     arrows: true,
-  //     autoplay: false, // Desabilita o autoplay
-  //     infinite: true
-  //   });
-  // }
-
-  ngAfterViewInit() {
+  initializeCarousels() {
     const petCarousels = this.elementRef.nativeElement.querySelectorAll('.pet-carousel');
 
     petCarousels.forEach((petCarousel: any) => {
@@ -124,14 +46,18 @@ export class MainScreenComponent implements OnInit, AfterViewInit{
     });
   }
 
-  getPets(): Observable<any[]> {
-    return this.petService.getPets().pipe(
-      map((res: any) => {
-        if (res && Array.isArray(res)) {
-          console.log(res)
-          return res;
-        } else return [];
+  getPets() {
+    this.loadingPets = true;
+    this.petService.getPets().pipe(take(1))
+      .subscribe({
+        next: response => {
+          this.listPets = response;
+          this.loadingPets = false;
+
+          this.cdr.detectChanges();
+          this.initializeCarousels()
+          
+        }
       })
-    ) as Observable<any[]>;
   }
 }

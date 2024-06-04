e534230ed1ae9b3f28465e1325747474f379737d
laura.souza <laura.souza@gclaims.com.br>
2024-06-04 00:18:45 -0300

    empty lists

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -6,6 +6,8 @@ declare var $: any;
 import 'slick-carousel';
 import { ActivatedRoute } from '@angular/router';
 import { LoaderService } from '../services/loader/loader.service';
+import { UserService } from '../services/user/user.sevice';
+import { AuthService } from '../services/auth/auth.service';
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
@@ -16,18 +18,37 @@ export class MainScreenComponent implements OnInit {
   isProfile: boolean = false;
   isFavorite: boolean = false;
   isSubscribes: boolean = false;
+  userId!: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    public loaderService: LoaderService
+    public loaderService: LoaderService,
+    private userService: UserService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.userId = this.authService.getUserId();
+
     this.activatedRoute.queryParams.subscribe((res: any) => {
       this.isProfile = res.isProfile == "true" || res.isProfile == true ? true : false;
       this.isFavorite = res.isFavorite == "true" || res.isFavorite == true ? true : false;
       this.isSubscribes = res.isSubscribes == "true" || res.isSubscribes == true ? true : false;
     });
+
+    console.debug('userId => ',this.userId)
+    if(this.userId) {
+      this.getNotifications();
+    }
+  }
+
+
+  getNotifications() {
+    this.userService.getNotificationsByUser(this.userId).pipe(take(1)).subscribe({
+      next: (response: any) => {
+        console.debug('response => ', response)
+      }
+    })
   }
 
 }
